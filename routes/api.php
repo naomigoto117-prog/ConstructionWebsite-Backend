d8dfe5dd005815d69c6140c5e73d39ffdf818702
Naomi <naomi.goto117@gmail.com>
2026-07-02 12:25:03 +0800

    Add contact API route

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,18 @@ Route::get('/test', function () {
     return response()->json([
         'message' => 'Laravel API is working!'
     ]);
+});F
+
+Route::post('/contact', function (Illuminate\Http\Request $request) {
+    $validated = $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|email',
+        'subject' => 'required|string|max:255',
+        'message' => 'required|string',
+    ]);
+
+    return response()->json([
+        'message' => 'Contact message received!',
+        'data' => $validated
+    ]);
 });
