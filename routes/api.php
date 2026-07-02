3969ce45daf5dbdd6018b39da5b9cd35554558dd
Naomi <naomi.goto117@gmail.com>
2026-07-02 12:13:33 +0800

    Add API test route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
 
-Route::post('/contact', [ContactController::class, 'send']);
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'Laravel API is working!'
+    ]);
+});
